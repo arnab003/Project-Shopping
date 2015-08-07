4ee254c6ac4bfe7a6afa878722f23762262f8694
subhasisghosal <subhasis@fusioncharts.com>
2015-08-07 09:30:42 +0530

    all files added

---
+++ bag.js
@@ -0,0 +1,204 @@
+(function WrapBags(){
+	//"use strict";
+	// var Inventory = core.getLib("InventoryItems");
+	var inventory = core.getLib("InventoryItems"),
+	Inventory = new inventory(),
+	productOptionData =	core.getLib("productOptionData"),
+	AddDataEventLister = core.getLib("AddDataEventLister"),
+	selectedElement = core.getLib("selectedElement");
+	groceryBag = new Bag(),
+	beverageBag = new Bag(),
+	obj= {
+		"grocery": groceryBag.selectedData,
+		"bev" : beverageBag.selectedData
+	},
+	flag=0;
+
+	productOptionData.renderProductDetails();
+
+	/*function AddDataEventLister(tagId, addFucnctionReferece, event)
+	{
+		var add=document.getElementById(tagId);
+		add.addEventListener(event, addFucnctionReferece);
+	}
+	*/	
+	
+	/*{change: function (){console.log('test');}}*/
+
+	AddDataEventLister("category",{change: changeSelection} );
+
+	/*function selectedElement(id){
+		return document.getElementById(id).options[document.getElementById(id).selectedIndex];
+	}*/
+
+	/*function update(name,price,quantity){
+		if(quantity !== "undefined"){
+			document.getElementById("qty").value = quantity;	
+		}
+		if(name !== "undefined"){
+			document.getElementById("name").value = name;	
+		}
+		if(price !== "undefined"){
+			document.getElementById("price").value = price;	
+		}
+		
+	}*/
+
+	// function productOptionData(name,price,quantity){
+	// 	if(typeof quantity === "undefined"){
+	// 		quantity = 0;
+	// 	}
+	// 	document.getElementById("name").value = name;
+	// 	document.getElementById("price").value = price;
+	// 	document.getElementById("qty").value = quantity;
+	// }
+
+	function changeSelection(){
+		
+
+		var id = selectedElement("items").id;
+		var quantity = 1,
+		name = Inventory.getItemById(id).getName(),
+		price = Inventory.getItemById(id).getPrice();
+
+		/*if(flag === 0){
+			productOptionData(name,price,quantity);
+			flag=1;
+		}
+		else
+			update(name,price,quantity);	
+*/
+		productOptionData.productInputSet(name,price,quantity);
+
+		if(selectedElement("category").value == "grocery"){
+			// AddDataEventLister("items",groceryBag.change,"change");
+			AddDataEventLister("items",{change: groceryBag.change });
+			//AddDataEventLister("add",groceryBag.add,"click");
+			AddDataEventLister("add",{click : groceryBag.add});
+			selectedData = obj.grocery;
+		}
+		else{
+			//AddDataEventLister("items",beverageBag.change,"change");
+			AddDataEventLister("items",{change: beverageBag.change });
+			// AddDataEventLister("add",beverageBag.add,"click");
+			AddDataEventLister("add",{click : beverageBag.add});
+			selectedData = obj.bev;
+		}
+		for(var keys in selectedData)
+		{
+			if(selectedData[keys].id == id)
+			{
+				quantity = selectedData[keys].quantity;
+				//update(name,price,quantity);
+				productOptionData.productInputSet(name,price,quantity);
+				//break;
+			}
+		}
+
+		//productOptionData(name,price,quantity);
+	}
+
+	function Bag()
+	{
+		this.selectedData = []; 
+
+		var cart=core.getLib("cart");
+
+		this.add= function()
+		{
+			var upperLimit, lowerLimit;
+			if(selectedElement("category").value === "grocery"){
+				selectedData = obj.grocery;
+				// upperLimit = 6;
+				// lowerLimit = 2;
+			}
+			else{
+				selectedData = obj.bev;
+				// upperLimit = 4;
+				// lowerLimit = 2;
+			}
+			var flag=0;
+			var id = selectedElement("items").id;
+			for(var keys in selectedData)
+			{
+				if(selectedData[keys].id == id)
+				{
+					// total = document.getElementById("qty").value + this.calculate_total_item(selectedData);
+					// if(total >= lowerLimit && total <= upperLimit){
+						
+						selectedData[keys].quantity = productOptionData.productsInputGetIds().qty; 
+						flag=1;	
+					//}
+					/*else{
+						alert("Sorry your bag is already filled! Please empty some of your cart before checkout");
+					}*/
+					
+				}
+			}
+
+			if(flag === 0)
+			{
+				var newData = {};
+
+				newData.name = Inventory.getItemById(id).getName();
+				newData.id = id;
+				newData.price = Inventory.getItemById(id).getPrice();
+				newData.quantity = productOptionData.productsInputGetIds().qty;
+				
+				if(typeof selectedData === "undefined")
+					selectedData = [];
+				selectedData.push(newData);
+			}
+			
+			console.log(obj);
+			cart.addBagInCart(obj);
+			cart.calculateAmount();
+		};
+
+
+		this.change = function()
+		{
+			var id = selectedElement("items").id,
+			name = Inventory.getItemById(id).getName(),
+			price = Inventory.getItemById(id).getPrice(),
+			quantity=1;
+
+			if(selectedElement("category").value === "grocery"){
+				selectedData = obj.grocery;
+			}
+			else{
+				selectedData = obj.bev;
+			}
+
+			for(var keys in selectedData)
+			{
+				if(selectedData[keys].id == id)
+				{
+					quantity = selectedData[keys].quantity; 
+					
+				}
+			}
+			
+			productOptionData.productInputSet(name,price,quantity);
+
+			/*var id = document.getElementById("items").options[document.getElementById("items").selectedIndex].id;
+			
+			/*name = Inventory.getItemById(id).getName();
+			price = Inventory.getItemById(id).getPrice();
+			// productOptionData(name,price,quantity);
+			*/
+		};
+
+		/*this.calculate_total_item = function(selectedData){
+			var total = 0;
+			for(var keys in selectedData){
+				total = total + selectedData.quantity;
+			}
+			alert(total);
+			return total;
+		}*/
+	}	
+	
+})();
+
+
---
+++ inventory.js
@@ -0,0 +1,208 @@
+(function(){
+
+	//item function to create items
+	function Item(name,id,price){
+
+		this.name=name;
+		this.id=id;
+		this.price=price;
+
+	}
+
+	Item.prototype.getName=function(){
+
+		return this.name;
+	};			
+
+	Item.prototype.getId=function(){
+
+		return this.id;
+		
+	};	
+	Item.prototype.getPrice=function(){
+
+		return this.price;
+
+	};					
+
+	//Inventory items function
+	var InventoryItems=function(){
+		
+		var i,itemCatalog,name,id,price,item;
+		var categories=[];
+		this.items={};
+		//storing the item catalog to a variable
+		itemCatalog=catalogData;
+		// console.log(itemdata);
+		for(i in itemCatalog){
+		
+			for(var j in itemCatalog[i]){
+			
+				name=itemCatalog[i][j].name;
+				id=itemCatalog[i][j].id;
+				price=itemCatalog[i][j].price;
+				console.log(name+" "+id+" "+price);
+				item=new Item(name,id,price);
+				categories.push(item);
+			
+		
+			}
+			this.items[i]=categories;
+		categories=[];
+		}
+/*		console.log(this.items);
+		console.log(this.items["grocery"]);
+		var grocery=[],beverages=[];
+
+		grocery=[new Item("Rice","r1",200),new Item("Oil","o1",300)];
+
+		beverages=[new Item("Tea","t1",200),new Item("Coffee","c1",200)];
+
+		this.items.grocery=grocery;
+
+		this.items.beverages=beverages;*/
+
+	};
+
+	InventoryItems.prototype.getAllItems=function(){
+		var j=0;
+		//console.log(items);
+		return this.items;
+	};
+
+	InventoryItems.prototype.getItemById=function(id){
+
+		var item,j,category;
+		j=0;
+		
+		for(var i in Object.keys(this.items))
+		{
+			var keyname=Object.keys(this.items)[i];
+			var object=this.items[keyname];
+			for(j in object){
+				
+			//	console.log(object[j].getId());
+				if(object[j].getId()==id){
+					return object[j];
+				}
+
+			}
+		
+
+		}
+
+	};	
+
+	function grocery(){
+
+		var tax=12.5;
+
+		this.getVat=function(){
+		return tax;
+		};
+	}
+
+	grocery.prototype=Object.create(InventoryItems);
+    
+	function beverages(){
+
+		var tax=11.5,additonaltax=2.5;
+
+		this.getVat=function(){
+									
+			return tax;
+
+		};
+
+		this.getAdditionalVat=function(){
+									
+			return additonaltax;
+
+		};
+	}
+
+	beverages.prototype=Object.create(InventoryItems);
+		
+		// var beverageObject=new beverages();
+		// var tax=beverageObject.getAdditionalVat();
+    function PopulateList(){
+
+		var i,items,itemlist,categoryOptions;
+		items=inventoryObject;
+
+		itemlist=items.getAllItems();
+		categoryOptions=[];
+		categoryOptions.push({name:"Select Category"});
+		
+		for(i in Object.keys(itemlist)){
+
+			categoryOptions.push({name:Object.keys(itemlist)[i]});
+		}
+
+		var onChangeEvent={
+
+			change:PopulateItems
+
+		};
+
+
+
+    							
+    	var createOptions=core.getLib("createOptions");
+    						//console.log(categoryOptions[0].name);
+    	createOptions("category",categoryOptions,onChangeEvent);
+    						// createOptions("items","select");
+    	var itemList=document.getElementById('items');
+
+		function PopulateItems(){
+					
+			var itemOptions=[];
+
+			if(document.getElementById('items'))
+				document.getElementById('items').remove();
+			
+			var category=document.getElementById('category').value;
+			/*console.log(category);
+		 	var keyname=Object.keys(itemlist)[i];*/
+			var object=itemlist[category];
+		/*	console.log(object);
+			// console.log(itemlist);
+			// console.log(object[0].getName());*/
+
+			for(var j in object){
+				
+				itemOptions.push({id:object[j].getId(),name:object[j].getName()});
+
+			}
+			// console.log(itemOptions[0]);
+			
+					
+			createOptions("items",itemOptions);
+
+					
+		}
+
+                		
+	}
+    		
+    var inventoryObject=new InventoryItems();
+    var groceryObject=new grocery();
+    var beverageObject=new beverages();
+    //console.log(inventoryObject.getItemById('r1').getPrice());
+	var registerApi=function(){
+
+		var KEY = "";
+		KEY = core.getKeys("ronojit");
+		
+		var out=core.setLib(KEY, "InventoryItems", InventoryItems);
+		core.setLib(KEY,"grocery",grocery);
+		core.setLib(KEY,"beverages",beverages);
+		console.log(out);
+		PopulateList();
+
+	};
+
+	//Calling Register Api which is registering all the class references
+	registerApi();
+
+})();
